b3045f339fc1a307d23f9b8ee700bd01d5cc6f12
JTipton25 <JTipton25@gmail.com>
2016-09-22 16:31:50 -0600

    updated data/observer.RData and added raster files to load data

--- R/prior-for-pooled-nu.R
+++ R/prior-for-pooled-nu.R
@@ -1,8 +1,9 @@
-layout(matrix(1:3, 1, 3))
+layout(matrix(1:4, 2 ,2))
 mu=rbeta(1000, 5,5)
 hist(mu)
 eta=rgamma(1000,10,0.1)
 hist(eta)
 alpha=mu*eta
 beta=(1-mu)*eta
-hist(2/rbeta(1000, alpha, beta))
+hist(2/rbeta(1000, alpha, beta), breaks=100)
+hist(rgamma(1000, 2, 0.1), breaks=100)
